be96d27636b427d0148bbd0937ce105942a10a3a
Douglaskm91 <51854587+Douglaskm91@users.noreply.github.com>
2020-12-02 11:47:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-function removerLetrasRepetidas(palavra) {
+/* Remover letras repetidas (JS-HTML)*/
+function removerLetrasRepetidas() {
+    var palavra = document.getElementById("palavra").value;
+    var letrasUnicas = '';
+    for (var i = 0; i < palavra.length; i++) {
+        if (palavra.lastIndexOf(palavra[i]) == palavra.indexOf(palavra[i])) {
+            letrasUnicas += palavra[i];
+        }
+    }
+    document.getElementById("novaPalavra").innerHTML = "Nova palavra: " + letrasUnicas;
+}
+
+/* Letras invertidas (JS-HTML)*/
+function inverterNome() {
+    var nome = document.getElementById("nome").value;
+    var nomeInvertido = '';
+    for (var i = nome.length - 1; i >= 0; i--) {
+        nomeInvertido += nome[i];
+    }
+    document.getElementById("novoNome").innerHTML = "Nome invertido: " + nomeInvertido;
+}
+
+
+
+/* --------------------------------------------------------------------------------------- */
+
+
+/* Remover letras repetidas (FuntionJS)(console.log) */
+
+function removerLetrasRepetidas2(palavra) {
     var letrasUnicas = '';
     for (var i = 0; i < palavra.length; i++) {
         if (palavra.lastIndexOf(palavra[i]) == palavra.indexOf(palavra[i])) {
@@ -8,17 +37,17 @@ function removerLetrasRepetidas(palavra) {
     return letrasUnicas;
 }
 
-console.log(removerLetrasRepetidas('baraban'));
-console.log(removerLetrasRepetidas('anaconda'));
+console.log(removerLetrasRepetidas2('baraban'));
+console.log(removerLetrasRepetidas2('anaconda'));
 
 
-/* Nome Invertido */
+/* Nome Invertido (FuntionJS)(console.log) */
 
-function inverterNome(nome) {
+function inverterNome2(nome) {
     var nomeInvertido = '';
     for (var i = nome.length - 1; i >= 0; i--) {
         nomeInvertido += nome[i];
     }
     return nomeInvertido;
 }
-console.log(inverterNome('Douglas'));
+console.log(inverterNome2('Douglas'));
